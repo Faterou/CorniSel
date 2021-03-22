3f812143af6993c543bbc621e8d91acaaa3f1d72
Sylvain Hallé <shalle@acm.org>
2021-03-22 10:52:06 -0400

    Testing for elaborations

--- Source/Demo/src/SimpleTest.java
+++ Source/Demo/src/SimpleTest.java
@@ -24,7 +24,7 @@ public class SimpleTest
 {
 	static
 	{
-		System.setProperty("webdriver.gecko.driver", "/home/sylvain/Desktop/geckodriver");
+		System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
 	}
 	
 	@Test
